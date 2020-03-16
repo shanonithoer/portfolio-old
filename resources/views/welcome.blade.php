38da115f74b467925c8e635674f2ade374f39c56
Shano Nithoer <49998299+deshanonithoer@users.noreply.github.com>
2020-03-16 01:38:08 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Ignas is prachtig.
                 </div>
-                <img src="../images/ignas.jpeg" alt="ignas"/>
+                <img src={{asset('images/ignas.png')}} alt="ignas"/>
             </div>
         </div>
     </body>
