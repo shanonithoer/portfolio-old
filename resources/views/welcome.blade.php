2e134d0c124cdb8f2a8912bc3483348e41470cd8
Shano Nithoer <49998299+deshanonithoer@users.noreply.github.com>
2020-03-16 01:22:05 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ignas is kanker gay.
+                    Ignas is gay.
                 </div>
             </div>
         </div>
